2f5f88f71df59f3d0672df0939d6b636b0ac9dce
Nitish Gadangi <nitishgadangi@gmail.com>
2024-02-20 17:25:42 +0530

    Major : updated web page with support for custom configuration

--- src/Utils/constants.ts
+++ src/Utils/constants.ts
@@ -1,3 +1,6 @@
+import { ParserConfiguration } from "../types/entities";
+import { DefaultParserConfig } from "./default_config";
+
 export const RegularExpressions = {
     IBOUTLET_VARNAME: (variableName: string) => {
         const basePattern: string = '.*@IBOutlet\\s+\\S+(\\s+\\S+)*\\s+';
@@ -8,4 +11,12 @@ export const RegularExpressions = {
 export const AnotationConstants = {
     IB_ACTION: '@IBAction',
     OBJC: '@objc'
+}
+
+export const DEFAULT_PARSER_CONFIG: ParserConfiguration = {
+    setupFunctionName: DefaultParserConfig.setupFunctionName,
+    tagsToIgnore: DefaultParserConfig.tagsToIgnore,
+    propertyDeclarationsToIgnore: DefaultParserConfig.propertyDeclarationsToIgnore,
+    propertiesToIgnore: DefaultParserConfig.propertiesToIgnore,
+    propertiesToCast: DefaultParserConfig.propertiesToCast
 }
--- website/main.ts
+++ website/main.ts
@@ -1,22 +1,50 @@
 import { Xib2Swift } from "../src/builders/xib2swift";
+import { ParserConfiguration } from "../src/types/entities";
+import { DEFAULT_PARSER_CONFIG } from "../src/utils/constants";
 
-const button = document.getElementById("generate-button") as HTMLButtonElement;
+const form = document.getElementById('converterForm') as HTMLFormElement;
+form.addEventListener('submit', generate);
 
-button.addEventListener("click", generate);
+const configurationElement = document.getElementById("configuration") as HTMLTextAreaElement;
+document.addEventListener('DOMContentLoaded', () => {
+    fetchAndDisplayConfiguration();
+});
 
-function generate() {
-    console.log("Generate");
-    const input = document.getElementById("input-Text") as HTMLTextAreaElement;
+function generate(event: Event) {
+    event.preventDefault()
+    console.log("Generating output...");
+    
+    const xibInputElement = document.getElementById("xib-code") as HTMLTextAreaElement;
     const swiftCodeElement = document.getElementById("swift-code") as HTMLTextAreaElement;
-    let xib = input.value;
+    const outputElement = document.getElementById("output-code") as HTMLTextAreaElement;
+
+    const configData = configurationElement.value;
+    let configuration: ParserConfiguration = {};
+    try {
+        configuration = JSON.parse(configData);
+    } catch (error) {
+        configuration = {};
+        console.log('Opps! Error while parsing provided configuration file. ');
+        console.error('Error log:', error);
+        console.log('Fear not! Going ahead with default configuration for now.');
+    }
+    let xibCode = xibInputElement.value;
     let swiftCode = swiftCodeElement.value;
-    const output = document.getElementById("output-Text") as HTMLTextAreaElement;
-    let xib2swift = new Xib2Swift(xib)
+    let xib2swift = new Xib2Swift(xibCode, configuration);
+
     let viewcode = '';
     if (swiftCode != '') {
         viewcode = xib2swift.convertWithSwiftFile(swiftCode);
     } else {
         viewcode = xib2swift.convert();
     }
-    output.value = viewcode;
+    outputElement.value = viewcode;
+}
+
+function fetchAndDisplayConfiguration() {
+    try {
+        configurationElement.value = JSON.stringify(DEFAULT_PARSER_CONFIG, null, 4);
+    } catch (error) {
+        console.error('Error fetching Parser Config:', error);
+    }
 }
